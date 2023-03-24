a8efcb588123810437f7232fb7a432fc8b6ab4b4
bwill210 <bwill210@fiu.edu>
2023-03-23 18:55:30 -0700

    completed 5 practice problems

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let i = 0;
+    while (i < arguments.length){
+        let index = array.indexOf(arguments[i]);
 
+        if (index >= 0) { // if item exists in array, remove it.
+            array.splice(index, 1);
+        }
+        i++;
+    }
+    return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) || 
+        num1 < 0 || num2 < 0)
+        return "ERROR";
+    if (num1 < num2){
+        sum = num1;
+        lowNum = num1;
+        highNum = num2;
+    }
+    else {
+        sum = num2;
+        lowNum = num2
+        highNum = num1
+    }
+    let i = 0;
+    let numToAdd = lowNum + 1;
+    while (i < highNum - lowNum) {
+        sum += numToAdd;
+        ++numToAdd;
+        ++i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
