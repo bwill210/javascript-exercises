58a3322c9c66546d45b25f7c80c4f739d6b51277
bwill210 <bwill210@fiu.edu>
2023-03-30 16:09:16 -0700

    completed leapLYears.js excercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,15 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0 && !(year % 100 == 0)) {
+        return true;
+    }
+    else if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
+        return true;
+    else return false;
 };
 
 // Do not edit below this line
+// divisible by 4
+// divisible by 100 and 400 = leap year
+// divisible by 100 but not 400 = not leap year
+
 module.exports = leapYears;
