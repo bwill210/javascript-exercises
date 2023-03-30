43cb7d2b6002377aff8068fdd618dff2f142040a
bwill210 <bwill210@fiu.edu>
2023-03-30 16:23:56 -0700

    completed excercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(number) {
+  let converted = (number - 32)*(5/9);
+  let result = Math.round(converted * 10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(number) {
+  let converted = number * (9/5) + 32;
+  let result = Math.round(converted * 10) / 10;
+  return result;
 };
 
 // Do not edit below this line
