238be7b63089d87a787c20d6352ed52d9da6a2e8
bwill210 <bwill210@fiu.edu>
2023-05-23 19:06:52 -0700

    completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,36 @@
-const palindromes = function () {
+/*
+const palindromes = function (string) {
+    const str = string.toLowerCase();
+    let lastIndex = str.length -1;
+    let i = 0;
+    let j = lastIndex;
+    while (i < j) {
+        if (str[i] == ' ' || str[i] == "'" || str[i] == "," || 
+            str[i] == "!" || str[i] == "?" || str[i] == ".") {
+            i++;
+            continue;
+        }
+        if (str[j] == ' ' || str[j] == "'" || str[j] == "," || 
+            str[j] == "!" || str[j] == "?" || str[j] == ".") {
+            j--;
+            continue;
+        }
+        if (str[i] == str[j]) {
+            i++;
+            j--;
+            continue;
+        }
+        else {
+            return false;
+        }
+    }   return true;
+};
+*/
 
+
+const palindromes = function (string) {
+    const processedStr = string.toLowerCase().replace(/[^a-z]/g, "");
+    return processedStr == processedStr.split("").reverse().join("");
 };
 
 // Do not edit below this line
