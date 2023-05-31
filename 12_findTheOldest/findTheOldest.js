03e01e2b7b12d8293cefabded8fcc574a78fa287
bwill210 <bwill210@fiu.edu>
2023-05-31 14:17:09 -0700

    completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentPerson) => {
+        let oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        let currentPersonAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return currentPersonAge > oldestAge ? currentPerson : oldest
+    })
 };
+
+
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
