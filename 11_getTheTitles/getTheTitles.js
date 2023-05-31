83122e6319f02f812b30a322c962a606c7a321d7
bwill210 <bwill210@fiu.edu>
2023-05-31 12:47:30 -0700

    completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const justTitles = books.map(book => book.title);
+    return justTitles;
 };
 
 // Do not edit below this line
